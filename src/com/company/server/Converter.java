dff957493e9f645b4e5c28eb5f138c25507ed988
Sergey Karashevich <karashevich@gmail.com>
2014-09-22 16:26:08 +0400

    Added JSON convert methods: Item -> JSON, JSON -> Item, SetOfItems -> JSON, JSON -> SetOfItems

--- src/com/company/server/Converter.java
+++ src/com/company/server/Converter.java
@@ -1,16 +1,79 @@
 package com.company.server;
 
 
+import com.company.structures.Item;
+import org.jetbrains.annotations.NotNull;
 import org.json.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
  * Created by jetbrains on 9/22/14.
  */
+//TODO: Change this JSONator with Jackson
 
 public class Converter {
 
-    public static void toJSON(){}
+    @NotNull
+    private static JSONObject itemToJSON(@NotNull Item item) throws JSONException {
+        return new JSONObject()
+                .put("link", item.getLink())
+                .put("login", item.getLogin())
+                .put("pass", item.getPass());
+    }
+
+    @NotNull
+    private static Item jsonToItem(@NotNull JSONObject jsonObject) throws JSONException {
+
+        return new Item(jsonObject.getString("link"), jsonObject.getString("login"),jsonObject.getString("pass"));
+    }
+
+    @NotNull
+    private static JSONObject itemsToJSON(@NotNull Set<Item> itemSet) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray();
+        for (Item item : itemSet) {
+            jsonArray.put(itemToJSON(item));
+        }
+        
+        return new JSONObject().put("items", jsonArray);
+    }
+
+    @NotNull
+    private static Set<Item> jsonToItems(@NotNull JSONObject jsonObject) throws JSONException {
+
+        JSONArray jsonArray = jsonObject.getJSONArray("items");
+        HashSet<Item> resultSet = new HashSet<Item>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            resultSet.add(jsonToItem(jsonArray.getJSONObject(i)));
+        }
+
+        return resultSet;
+    }
+
+
+
+    public static void main(String[] args) throws JSONException {
+
+        Item item1 = new Item("www.kremlin.ru", "Vladimir Putin", "putinka");
+        Item item2 = new Item("www.kremlin.org", "Dmitry Medvedev", "medved");
+        
+        HashSet<Item> set = new HashSet<Item>();
+
+        set.add(item1);
+        set.add(item2);
+
+        JSONObject jsonObject = itemsToJSON(set);
+
+        HashSet<Item> items = new HashSet<Item>(jsonToItems(jsonObject));
+
+        System.out.println(items);
+        //System.out.println(jsonObject.toString());
+        
+    }
 
 }
 
